c29cbae2226da385fd060c305deda73f83cc0a7b
Pankaj <panbhatt@gmail.com>
2022-03-05 21:07:14 -0500

    Added more files.

---
+++ first-multi-block/multipblock.tf
@@ -0,0 +1,8 @@
+
+output "hello1" {
+    value = "Pankaj Bhatt"
+}
+
+output "hello2" {
+    value = "Abhinandni Bhatt"
+}
---
+++ first/first.tf
@@ -0,0 +1,7 @@
+// This is the first terraform file. 
+
+#this is comment. 
+
+output hello1 {
+    value = "pankaj Bhatt"
+}
