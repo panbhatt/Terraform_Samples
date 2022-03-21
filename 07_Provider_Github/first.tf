74c283fed2c78517203d5d5d477848f173e6ba0e
Pankaj <panbhatt@gmail.com>
2022-03-20 23:41:05 -0400

    Added github token and apply terraform apply command

--- 07_Provider_Github/first.tf
+++ 07_Provider_Github/first.tf
@@ -5,7 +5,7 @@ https://registry.terraform.io/providers/integrations/github/latest/docs
     2. terraform init -> it will install the plugins. it will create .teeraform folder and lock file. The folder will contain
      all the plugin details.
     3. terraform plan -> will tell all the resources that needs to be created. 
-    4. terraform apply -> it would apply the plan in the provider. 
+    4. terraform apply -auto-approve -> it would apply the plan in the provider. 
 
 
 */
@@ -22,12 +22,12 @@ terraform {
 
 
 provider "github" {
-
+token =  "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"
 }
 
 resource "github_repository" "terrform_created_first_Repo_GSBM" {
   name = "TERRAFORM_GSBM_REPO"
-  description = "My First Github Repo from the terraform"
+  description = "My First Github Repo from the terraform Code .."
   visibility = "public"
   auto_init = true
 }
